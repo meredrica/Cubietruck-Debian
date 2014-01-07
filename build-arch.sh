b40bbaf886af6b73a575af7490e30b225ccdd815
meredrica <stuff@meredrica.org>
2014-01-07 12:15:17 +0100

    adds build-arch.s for requirement install in arch

---
+++ build-arch.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+SRC=$(pwd)
+set -e
+
+#Requires root ..
+if [ "$UID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+#--------------------------------------------------------------------------------
+# Downloading necessary files
+#--------------------------------------------------------------------------------
+echo "------ installing requirements"
+
+function aur {
+	if ! pacman -Qi >>/dev/null 2>&1 $1
+	then
+		wget https://aur.archlinux.org/packages/${1:0:2}/$1/$1.tar.gz
+		tar -xzf $1.tar.gz
+		cd $1
+		makepkg -s --asroot -f
+		FILE=`ls | grep .pkg`
+		pacman --noconfirm -U $FILE
+	else
+		echo installed: $1
+	fi
+}
+
+mkdir -p $SRC/aur
+cd $SRC/aur
+
+pacman --needed --noconfirm -S bison ccache flex gawk gettext git linux-headers linux lvm2 texinfo texlive-bin util-linux zlib unzip ncurses pkg-config libusb base-devel elfutils libmpc ppl gmp qemu
+
+
+aur cloog-ppl
+aur libmpc09
+aur binutils-arm-linux-gnueabihf-bin
+aur libgcc-4.7-dev-armhf-cross
+aur cpp-arm-linux-gnueabihf-bin
+aur gcc-arm-linux-gnueabihf-bin
+aur debootstrap
+aur uboot-mkimage
+#aur binfmt-support
+
+# call main build script
+
+$SRC/build.sh
